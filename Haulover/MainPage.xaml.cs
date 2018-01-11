5825ec9211cdbca319dbbd543a555b4aedc840a1
Marc-Andre Harbec <15015366+wolfmah@users.noreply.github.com>
2018-01-11 14:43:37 -0500

    Test how to do simple toast notifications and async UI update.

--- Haulover/MainPage.xaml.cs
+++ Haulover/MainPage.xaml.cs
@@ -1,30 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Core;
+using Windows.UI.Notifications;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Haulover
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// .
     /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            // Get a toast XML template
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText04);
+
+            // Fill in the text elements
+            XmlNodeList stringElements = toastXml.GetElementsByTagName("text");
+            for (int i = 0; i < stringElements.Length; i++)
+            {
+                stringElements[i].AppendChild(toastXml.CreateTextNode("Line " + i));
+            }
+
+            // Specify the absolute path to an image
+            String imagePath = "file:///C:\\Users\\maharbec\\source\\repos\\Haulover\\Haulover\\Assets\\Haulover-app-icon.png";
+            XmlNodeList imageElements = toastXml.GetElementsByTagName("image");
+            imageElements[0].Attributes.GetNamedItem("src").NodeValue = imagePath;
+
+            ToastNotification toast = new ToastNotification(toastXml);
+            toast.Activated += ToastActivatedAsync;
+            toast.Dismissed += ToastDismissed;
+            toast.Failed += ToastFailed;
+
+            // Show the toast. Be sure to specify the AppUserModelId on your application's shortcut!
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+
+        private void ToastActivatedAsync(ToastNotification sender, object e)
+        {
+            DisplayToastAction("The user activated the toast.");
+        }
+
+        private void ToastDismissed(ToastNotification sender, ToastDismissedEventArgs e)
+        {
+            String outputText = "";
+
+            switch (e.Reason)
+            {
+                case ToastDismissalReason.ApplicationHidden:
+                    outputText = "The app hid the toast using ToastNotifier.Hide";
+                    break;
+
+                case ToastDismissalReason.UserCanceled:
+                    outputText = "The user dismissed the toast";
+                    break;
+
+                case ToastDismissalReason.TimedOut:
+                    outputText = "The toast has timed out";
+                    break;
+            }
+
+            DisplayToastAction(outputText);
+        }
+
+        private void ToastFailed(ToastNotification sender, ToastFailedEventArgs e)
+        {
+            DisplayToastAction("The toast encountered an error.");
+        }
+
+        private async void DisplayToastAction(String toastAction)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                txtToastAction.Text = toastAction;
+            });
         }
     }
 }
